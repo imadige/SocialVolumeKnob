053618c05212d6edecf8605b5bf8ada79d2b84ad
ligi <ligi@ligi.de>
2017-04-01 17:51:52 +0200

    Initial printing of notice

--- app/src/main/java/org/ligi/socialvolumeknob/IntroActivity.kt
+++ app/src/main/java/org/ligi/socialvolumeknob/IntroActivity.kt
@@ -1,10 +1,19 @@
 package org.ligi.socialvolumeknob
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintManager
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_intro.*
+import net.glxn.qrgen.android.QRCode
+import java.io.ByteArrayOutputStream
 
 class IntroActivity : AppCompatActivity() {
 
@@ -12,6 +21,10 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_intro)
+
+        print_button.setOnClickListener {
+            doWebViewPrint()
+        }
 
         fab.setOnClickListener {
             val text = identifier_edittext.text
@@ -25,6 +38,46 @@ class IntroActivity : AppCompatActivity() {
                 startActivity(Intent(this, MainActivity::class.java))
             }
         }
+
+    }
+
+    var mWebView: WebView? = null
+    private fun doWebViewPrint() {
+        // Create a WebView object specifically for printing
+        val webView = WebView(this)
+        webView.setWebViewClient(object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String) = false
+
+            override fun onPageFinished(view: WebView, url: String) {
+                createWebPrintJob(view)
+                mWebView = null
+            }
+        })
+
+        val bos = ByteArrayOutputStream()
+        val qrCode = QRCode.from("https://t2m7u.app.goo.gl/?link=https://socialvolumeknob.firebaseapp.com/&apn=org.ligi.socialvolumeknob")
+        qrCode.bitmap().compress(Bitmap.CompressFormat.PNG, 100, bos)
+        bos.close()
+        val encode = Base64.encodeToString(bos.toByteArray(), Base64.DEFAULT)
+
+        // Generate an HTML document on the fly:
+        val htmlDocument = "<html><body><h1>Social Volume Knob</h1>" +
+                "<p><font size='24'>With this qr-code you can adjust the volume of user: ${identifier_edittext.text}<br/><br/>" +
+                "Scan the qr-code, install the app, adjust the volume, enjoy your time</font></p>" +
+                "<center><img width=\"66%\" src=\"data:image/png;base64,$encode\"/></center></body></html>"
+        webView.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null)
+
+        mWebView = webView
+    }
+
+    private fun createWebPrintJob(webView: WebView) {
+
+        val printAdapter = webView.createPrintDocumentAdapter()
+
+        val jobName = getString(R.string.app_name) + " Document"
+        val printManager = getSystemService(Context.PRINT_SERVICE) as PrintManager
+        printManager.print(jobName, printAdapter, PrintAttributes.Builder().build())
 
     }
 }
